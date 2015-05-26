944fb36161a92c7c2cdb8b475609ffecf0c5f3ee
Jan Pacner <pacnerjan@volny.cz>
2015-05-26 18:53:08 +0200

    add: skeleton of new description of OA (updated to the current law state)
    add: enquiry 2014 (content, template, schema, and script for input semi-automation)

---
+++ misc/enquiry/2014/add_enquiries.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+dir=responses
+pre=enquiry_response_
+post=.txt
+
+while true; do
+  [ -d "$dir" ] && {
+    # highest existing index
+    _idx="$(cd "$dir" && ls -1 | awk 'BEGIN {
+      x=length("'"$pre"'")
+      y=length("'"$post"'")
+    } {
+      sub("^.{"x"}", "", $0)
+      sub(".{"y"}$", "", $0)
+      print
+    }' | sort -n | tail -n 1)"
+  }
+  fname="$pre$((_idx +1))$post"
+
+  while printf 'Create new response sheet "%s"? y|n ' "$dir/$fname"; do
+    read _r
+    case "$_r" in
+      y|Y) break;;
+      n|N) exit;;
+      *) ;;
+    esac
+  done
+
+  [ -d "$dir" ] || mkdir "$dir" || exit $?
+  [ -e "$dir/$fname" ] && {
+    printf 'Naming schema mismatch: file "%s" already exists\n' \
+      "$dir/$fname" >&2
+    exit 1
+  }
+
+  cp template.txt "$dir/$fname"
+  nvim -c 'wincmd w | startinsert' -O schema.txt "$dir/$fname"
+done
