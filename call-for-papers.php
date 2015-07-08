dd05203d63b1bd8b0241f534392841d1d6412835
Jan Pacner <pacnerjan@volny.cz>
2015-07-08 02:58:01 +0200

    add: Call for Proposals
    add: tray with randomly switching partners logos
    
    change: restructure page_home
    
    add: logo/icon encouraging new potential partners
    
    fix: CSS, several URLs and some other minor programming stuff

--- call-for-papers.php
+++ call-for-papers.php
@@ -1,6 +1,7 @@
 <?php
 
-header('location: https://files.openalt.org/index.php/s/aEcInQIqsguKFlP/download');
+//header('location: https://files.openalt.org/index.php/s/aEcInQIqsguKFlP/download');
+header('location: https://openalt.cz/2015/cs/home.html#cfp');
 exit();
 
 ?>
---
+++ cfp-form.php
@@ -0,0 +1,6 @@
+<?php
+
+header('location: https://www.openalt.org/2015/cfp-form');
+exit();
+
+?>
